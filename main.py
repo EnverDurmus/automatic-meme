65eefd65da67cc4a77eb96e2d5e88973d99c2cec
EnverDurmus <tuu74101@temple.edu>
2025-11-17 15:38:21 -0500

    main.py added
    Added main.py for discord bot, wrote the code in to make the bot appear online.

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 import discord
+
+class Client(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+
+client = Client(intents=intents)
+client.run('MTQ0MDA3MDIzNTIyMTkxNzc1Nw.G4nK0N.RV7wCxs4acZrVjhqoJjwsjiek0xNMyqRuqxXWk')
+
